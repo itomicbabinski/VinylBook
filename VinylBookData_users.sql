03ca965bebbb80690dd43ae5e7b8ad3a9ff5825c
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-05-21 18:47:41 +0200

    Update VinylBookData_users.sql

---
+++ VinylBookData_users.sql
@@ -0,0 +1,8 @@
+insert into users (uname,ulastname ,uemail,uphone,uaddress) values 
+('Arsen','Turk','a.turk@gmail.com','091233334',''),
+('Igor','Tomić','i.tomic.babinski@gmail.com','0914691495',''),
+('Goran','Matoš','goran.matos@a1.hr','0914691496',''),
+('Goran','Pleše','goran.plese@a1.hr','0914691488',''),
+('Silvija','Turković','s.turkovic@vip.hr','0912345678',''),
+('Ivana','Mihanović','i.mihanovic99@gmail.hr','091233445566','');
+
