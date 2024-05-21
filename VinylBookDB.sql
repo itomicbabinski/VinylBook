0ba0065c4e3f314d214c7cc6a7a812728e157620
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-05-21 18:33:12 +0200

    Update VinylBookDB.sql

--- VinylBookDB.sql
+++ VinylBookDB.sql
@@ -51,11 +51,5 @@ alter table exchanges add foreign key (record_copy_id) references record_copyes(
 alter table exchanges add foreign key (album_id) references albums(id);
 
 
---select * from clanovi;
 
--- članovi grupe WP4
---insert into clanovi (grupa,polaznik) values
-(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),
-(1,11),(1,12),(1,13),(1,14),(1,15),(1,16),(1,17),(1,18),(1,19),(1,20),
-(1,21),(1,22),(1,23),(1,24),(1,25),(1,26),(1,27);
 
