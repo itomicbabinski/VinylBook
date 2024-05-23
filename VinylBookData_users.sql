33adb0ee01bb907f969ae52e4c4a472ea5c1796b
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-05-23 17:12:51 +0200

    Obrisano visak sql

--- VinylBookData_users.sql
+++
@@ -1,8 +0,0 @@
-insert into users (uname,ulastname ,uemail,uphone,uaddress) values 
-('Igor','Tomić','i.tomic.babinski@gmail.com','0914691495','Cetinačka 55 31000 Osijek'),
-('Arsen','Turk','a.turk@gmail.com','091233334','Valjevačka 3 31000 Osijek'),
-('Goran','Matoš','goran.matos@a1.hr','0914691496','Maratonska 2 10000 Zagreb'),
-('Goran','Pleše','goran.plese@a1.hr','0914691488','Maratekova 89 10000 Zagreb'),
-('Silvija','Turković','s.turkovic@vip.hr','0912345678','Samoborska 22 21000 Rijeka'),
-('Ivana','Mihanović','i.mihanovic99@gmail.hr','091233445566','Ivekova 32 10000 Zagreb');
-
